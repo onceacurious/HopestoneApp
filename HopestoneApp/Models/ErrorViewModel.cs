e9677b88eb78ca62ea2528e3a5b2d925dd600d43
Francis Deo Aurelio <francisdeoaurelio.fda@gmail.com>
2021-08-15 23:56:02 +0800

    Add project files.

---
+++ HopestoneApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HopestoneApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
